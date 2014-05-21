6e5081694c244f86250aafa09cb858da1a810769
Karl Heinz Marbaise <info@soebes.de>
2014-05-21 20:06:52 +0200

    - Improved.

--- mod-version/src/test/java/com/soebes/training/domain/NewVersion.java
+++ mod-version/src/test/java/com/soebes/training/domain/NewVersion.java
@@ -1,5 +1,67 @@
 package com.soebes.training.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NewVersion {
+
+    //@formatter:off
+    private static final Pattern VERSION_PATTERN = Pattern.compile("(.*)" 
+        + "\\-((\\d+)(\\.(\\d+))*)" 
+        + "(\\-([^.]+))?" // Classifier
+        + "\\.((.+)(\\.(.+))*)$", // Extension .tar.gz ?
+        Pattern.CASE_INSENSITIVE);
+    //@formatter:on
+
+    private String version;
+    private String classifier;
+    private String extension;
+    private String artifact;
+
+    public NewVersion(String version) {
+        Matcher matcher = VERSION_PATTERN.matcher(version);
+        if (matcher.matches()) {
+            this.artifact = matcher.group(1);
+            this.version = matcher.group(2);
+            this.classifier = matcher.group(7);
+            this.extension = matcher.group(9);
+            //
+        } else {
+            throw new IllegalArgumentException("The format of the version does not match.");
+        }
+
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getClassifier() {
+        return classifier;
+    }
+
+    public void setClassifier(String classifier) {
+        this.classifier = classifier;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getArtifact() {
+        return artifact;
+    }
+
+    public void setArtifact(String artifact) {
+        this.artifact = artifact;
+    }
 
 }
--- mod-version/src/test/java/com/soebes/training/domain/NewVersionTest.java
+++ mod-version/src/test/java/com/soebes/training/domain/NewVersionTest.java
@@ -2,56 +2,55 @@ package com.soebes.training.domain;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NewVersionTest {
 
+    public static final String NO_CLASSIFIER = null;
+
     @DataProvider
     public Object[][] getVersions() {
-	//@formatter:off
-	return new String[][] {
-		{ "A-1.jar" },
-		{ "A-1.2.jar" },
-		{ "A-1.2.3.jar" },
-		{ "A-1.2.3.4.jar" },
-		{ "A-1.2.3.4.5.jar" },
-		{ "A-1.12.123.1234.12345.1234567.12345678.123456789.jar" },
-		{ "A-2.16.0.1.jar" },
-		{ "first-ear-1.2.3.4.5.jar" },
-		{ "xyz-abc-1.2.3.jar" },
-		{ "xyz-abc-1.2.3.45.jar" },
-		{ "xyz-abc-1.2.3.45.600.jar" },
-		{ "A-1-sources.jar" },
-		{ "A-1.2-test-jar.jar" },
-		{ "first-1.2-test-jar.tar.gz" },
-		{ "first-1.2-test-jar.anton.egon.friedhelm.gz" },
-		{ "first-1.2-test-jar.tar" },
-		{ "first-1.2-test-jar.tar.bz2" },
-		{ "first-1.2-test-jar.zip" },
-		{ "anton-1.2.4.5.tar.gz" },
-	};
-	//@formatter:on
+        //@formatter:off
+		return new String[][] {
+			{ "A",         "1",                                              NO_CLASSIFIER,  "jar"  },
+			{ "A",         "1.2",                                            NO_CLASSIFIER,  "jar"   },
+			{ "A",         "1.2.3",                                          NO_CLASSIFIER,  "jar"   },
+			{ "A",         "1.2.3.4",                                        NO_CLASSIFIER,  "jar"  },
+			{ "A",         "1.2.3.4.5",                                      NO_CLASSIFIER,  "jar"   },
+			{ "A",         "1.12.123.1234.12345.1234567.12345678.123456789", NO_CLASSIFIER,  "jar"   },
+			{ "A",         "2.16.0.1",                                       NO_CLASSIFIER,  "jar"   },
+			{ "first-ear", "1.2.3.4.5",                                      NO_CLASSIFIER,  "jar"   },
+			{ "xyz-abc",   "1.2.3",                                          NO_CLASSIFIER,  "jar"   },
+			{ "xyz-abc",   "1.2.3.45",                                       NO_CLASSIFIER,  "jar"   },
+			{ "xyz-abc",   "1.2.3.45.600",                                   NO_CLASSIFIER,  "jar"   },
+			{ "A",         "1",                                              "sources",      "jar" },
+			{ "A",         "1.2",                                            "test-jar",     "jar"  },
+			{ "first",     "1.2",                                            "test-jar",     "tar.gz"  },
+			{ "first",     "1.2",                                            "test-jar",     "anton.egon.friedhelm.gz"   },
+			{ "first",     "1.2",                                            "test-jar",     "tar"  },
+			{ "first",     "1.2",                                            "test-jar",     "tar.bz2"  },
+			{ "first",     "1.2",                                            "test-jar",     "zip"  },
+			{ "anton",     "1.2.4.5",                                        NO_CLASSIFIER,  "tar.gz"  },
+		};
+   		//@formatter:on
     }
-
-    private static final Pattern VERSION_PATTERN = Pattern.compile("(.*)"
-    	+ "\\-((\\d+)(\\.(\\d+))*)"
-    	+ "(\\-([^.]+))?" //Classifier
-    	+ "\\.((.+)(\\.(.+))*)$", //Extension .tar.gz ?
-	    Pattern.CASE_INSENSITIVE);
 
     @Test(dataProvider = "getVersions")
-    public void xTest(String version) {
-	Matcher matcher = VERSION_PATTERN.matcher(version);
-	assertThat(matcher.matches()).isTrue();
-	System.out.println("-- Version: " + version);
-	System.out.println("  Groups:" + matcher.groupCount());
-	for (int i = 0; i < matcher.groupCount(); i++) {
-	    System.out.println("  Group[" + i + "]=" + matcher.group(i));
-	}
+    public void shouldParseAllGivenVersionsWithoutFailure(String expectedArtifact, String expectedVersion,
+            String expectedClassifier, String expectedExtension) {
+        String createdVersion = expectedArtifact + "-" + expectedVersion
+                + (expectedClassifier == NO_CLASSIFIER ? "" : "-" + expectedClassifier) + "." + expectedExtension;
+        NewVersion version = new NewVersion(createdVersion);
+        assertThat(version.getArtifact()).isEqualTo(expectedArtifact);
+        assertThat(version.getVersion()).isEqualTo(expectedVersion);
+        assertThat(version.getClassifier()).isEqualTo(expectedClassifier);
+        assertThat(version.getExtension()).isEqualTo(expectedExtension);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentException() {
+        new NewVersion("anton");
+        // intentionally no assert() cause we expect to get an exception.
+    }
 }
